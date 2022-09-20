a5a567abc38816c46d28a39c3fe60a6037c679e1
unknown <nnawaf103@gmail.com>
2022-09-20 19:42:34 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import Nav from "./componenet/Nav.js";
 import Cards from "./componenet/Cards";
 import Slidebar from "./componenet/Slidebar";
 import Footer from "./componenet/Footer";
+import { Routes, Route } from "react-router-dom";
+import Setting from "./componenet/Setting";
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <Slidebar></Slidebar>
-      <Cards></Cards>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Slidebar />
+              <Cards />{" "}
+            </>
+          }
+        />
+        <Route path="/Setting" element={<Setting />}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/componenet/Nav.js
+++ src/componenet/Nav.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 function Mynav() {
   return (
@@ -11,10 +12,14 @@ function Mynav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link>
+              <Link to={"/"}>Home</Link>
+            </Nav.Link>
             <Nav.Link href="#link">Cart</Nav.Link>
             <NavDropdown title="Profile" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">setting</NavDropdown.Item>
+              <Link to={"/Setting"}>
+                <NavDropdown.Item href="#action/3.1">setting</NavDropdown.Item>
+              </Link>
               <NavDropdown.Item href="#action/3.2">Track</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Orders</NavDropdown.Item>
               <NavDropdown.Divider />
